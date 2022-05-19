1cc7cb278bf77e22a6190e9bc4ee4feda1efecd9
Jakub Kliś <jakubklis2201@gmail.com>
2022-05-19 10:33:29 +0200

    Ribbon Control cd

---
+++ Sklep/Sklep/Views/Usuwanie_dostawcy.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sklep.Views
+{
+    /// <summary>
+    /// Interaction logic for Usuwanie_dostawcy.xaml
+    /// </summary>
+    public partial class Usuwanie_dostawcy : UserControl
+    {
+        public Usuwanie_dostawcy()
+        {
+            InitializeComponent();
+        }
+    }
+}
